e412a58b49f2bd63db74b651ee465c35933b373b
Shivam <shivamchandel196@gmail.com>
2026-04-22 00:23:51 +0530

    connect with firebase and create OAuth functionality

---
+++ frontend/src/components/OAuth.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { app } from "../firebase";
+
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+} from "firebase/auth";
+
+const OAuth = () => {
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider);
+      console.log(result.user); 
+    } catch (err) {
+      console.log("Could not sign in with Google", err);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleGoogleClick}
+      type="button"
+      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+    >
+      Continue with Google
+    </button>
+  );
+};
+
+export default OAuth;
