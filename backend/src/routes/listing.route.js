617fb587ff8f71595ca4f8d9550e6d231f1b693a
Shivam <shivamchandel196@gmail.com>
2026-04-26 23:23:01 +0530

    Listing route fix

--- backend/src/routes/listing.route.js
+++ backend/src/routes/listing.route.js
@@ -1,21 +1,53 @@
-import express from'express';
-import { verifyToken } from '../utils/verifyUser.js';
-import { createlisting,getListing,getListings,updateListing,deleteListing } from "../controllers/listing.controller.js";
+import express from "express";
 
+import { verifyToken } from "../utils/verifyUser.js";
 
+import {
+  createlisting,
+  getListing,
+  getListings,
+  updateListing,
+  deleteListing,
+} from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
+/* CREATE LISTING */
 
+router.post(
+  "/create",
+  verifyToken,
+  createlisting
+);
 
+/* GET ALL LISTINGS */
 
+router.get(
+  "/get",
+  getListings
+);
 
+/* GET SINGLE LISTING */
 
+router.get(
+  "/get/:id",
+  getListing
+);
 
+/* UPDATE LISTING */
 
-router.delete("/delete/:id", verifyToken, deleteListing);
-router.post("/update/:id", verifyToken, updateListing);
-router.get("/get/:id", getListing);
-router.get("/get", getListings);
-router.post('/create',verifyToken,createlisting);
+router.post(
+  "/update/:id",
+  verifyToken,
+  updateListing
+);
+
+/* DELETE LISTING */
+
+router.delete(
+  "/delete/:id",
+  verifyToken,
+  deleteListing
+);
+
 export default router;
