210c24f0f4b724cf06b263239a1b7bdc1a7a373d
Shivam <shivamchandel196@gmail.com>
2026-04-27 14:07:41 +0530

    home listing.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -31,9 +31,11 @@ export default function Home() {
     const fetchListings = async () => {
       try {
         const [offerRes, rentRes, saleRes] = await Promise.all([
-          fetch("/api/listing/get?offer=true&limit=4"),
-          fetch("/api/listing/get?type=rent&limit=4"),
-          fetch("/api/listing/get?type=sale&limit=4"),
+    fetch(`${import.meta.env.VITE_API_URL}/api/listing/get?offer=true&limit=4`),
+
+fetch(`${import.meta.env.VITE_API_URL}/api/listing/get?type=rent&limit=4`),
+
+fetch(`${import.meta.env.VITE_API_URL}/api/listing/get?type=sale&limit=4`),
         ]);
 
         const [offerData, rentData, saleData] = await Promise.all([
